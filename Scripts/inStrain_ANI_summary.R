457a96a9364b5ba308a94194c2fe99ee04fdf3b6
Keith Bouma-Gregson <kbg@berkeley.edu>
2021-01-04 05:21:37 -0800

    Finalize ANI_summary.R script

--- Scripts/inStrain_ANI_summary.R
+++ Scripts/inStrain_ANI_summary.R
@@ -1,7 +1,6 @@
-## Format and join data from inStrain, dRep, and spatial relationships
+## Format and join data from inStrain and spatial data
 
-
-#setwd("/Users/kbg/Documents/UC_Berkeley/CyanoMeta_NSF/Metagenomics/Microcoleus_Analysis/GenomesData/Strains")
+library(tidyverse)
 
 
 #### INPUT DATA ####
@@ -33,15 +32,15 @@ sp1_sites <- read_tsv(file.path("Data/inStrain_data/", "inStrain_sample_species_
   select(-multiple_species)
 
 ## Nucleotide diversity (generated in inStrain_format_output.R) OK
-nuc_div <- read_tsv(file.path("Data/inStrain_data/", "nuc_div_summary.txt")) %>% 
+nuc_div <- read_tsv(file.path("Data/inStrain_data/", "nuc_div_summary_TEST.txt")) %>% 
   filter(species == "species_1") %>% 
   select(site, mean_pi, median_pi)
 
 ## SNV Genome summary data (generated in inStrain_format_output.R) OK
 snv_genomes <- read_tsv("Data/inStrain_data/snvs_genome_summary_TEST.tsv")
 
-## inStrain compare results
-comp_sp1 <- read_tsv("inStrain/instrainComparer_NoLoc_comparisonsTable_sp1.tsv") %>% 
+## inStrain compare results OK
+comp_sp1 <- read_tsv("Data/inStrain_data/compare_module_output_sp1_comparisonsTable.tsv") %>% 
   mutate(name1= str_replace(name1, "^.*-vs-", ""),
          name2= str_replace(name2, "^.*-vs-", "")) %>% 
   mutate(name1= str_replace(name1, ".sorted.bam", ""),
@@ -64,8 +63,8 @@ comp_sp1 <- read_tsv("inStrain/instrainComparer_NoLoc_comparisonsTable_sp1.tsv")
 
 comp_sp1.F <- comp_sp1 %>% 
   filter(percent_genome_compared > 0.25) %>% 
-  mutate(frac_popSNPs= population_SNPs / consensus_SNPs,
-         frac_ANI= popANI  /conANI) # fraction of consensus_SNPs that are population_SNPs
+  # Calculate pMA
+  mutate(pMA= 1 - (population_SNPs / consensus_SNPs)) # shared minor alleles: 1 - fraction of consensus_SNVs that are also population_SNVs
 
 #### JOIN DATA ####
 
@@ -76,8 +75,9 @@ ani_sum <- comp_sp1.F %>%
             mean_conANI= mean(conANI),
             sum_pop_sites= sum(population_SNPs),
             sum_con_sites= sum(consensus_SNPs),
-            frac_popSNPs= sum_pop_sites/sum_con_sites,
-            #mean_frac_popSNPs= mean(frac_popSNPs, na.rm=TRUE),
+            #frac_popSNVs= sum_pop_sites/sum_con_sites,
+            pMA= sum_pop_sites/sum_con_sites,
+            #mean_frac_popSNVs= mean(frac_popSNVs, na.rm=TRUE),
             sd_popANI= sd(popANI),
             sd_conANI= sd(conANI),
             median_popANI= median(popANI),
@@ -86,16 +86,12 @@ ani_sum <- comp_sp1.F %>%
   # WATERSHED AREA JOIN
   left_join(., watershed.area, by= c("name1" = "ggkbase_id")) %>% 
   left_join(., watershed.area, by= c("name2" = "ggkbase_id")) %>% 
-  rename(watershed_1= watershed_km2.x, watershed_2= watershed_km2.y) %>% 
-  # DREP ANI JOIN
-  #left_join(., sp1.ani) %>% 
+  rename(watershed.1= watershed_km2.x, watershed.2= watershed_km2.y) %>% 
+  mutate(watershed_diff= abs(watershed.2 - watershed.1)) %>% 
   # SNV_MBP JOIN
-  left_join(., snv_genomes, by= c("name1" = "ggkbase_id")) %>% 
-  left_join(., snv_genomes, by= c("name2" = "ggkbase_id")) %>% 
-  rename(pop_age.1= pop_age.x, pop_age.2= pop_age.y,
-         SNV_mbp.1= SNV_mbp.x, SNV_mbp.2= SNV_mbp.y) %>% 
-  mutate(pop_age_pair= str_c(pop_age.1, pop_age.2, sep= "-")) %>%
-  mutate(pop_age_pair= ifelse(pop_age_pair == "Young-Old", "Old-Young", pop_age_pair)) %>% 
+  left_join(., select(snv_genomes, ggkbase_id, SNV_mbp), by= c("name1" = "ggkbase_id")) %>% 
+  left_join(., select(snv_genomes, ggkbase_id, SNV_mbp), by= c("name2" = "ggkbase_id")) %>% 
+  rename(SNV_mbp.1= SNV_mbp.x, SNV_mbp.2= SNV_mbp.y) %>% 
   # RIVER DISTANCE JOIN
   left_join(., ani.riv.dist, by= c("name1", "name2")) %>% 
   left_join(., ani.riv.dist, by= c("name1" = "name2", "name2" = "name1")) %>% 
@@ -109,10 +105,9 @@ ani_sum <- comp_sp1.F %>%
   mutate(pi_diff_mean = abs(mean_pi.1 - mean_pi.2),
          pi_avg_mean = rowMeans(cbind(.$mean_pi.1, .$mean_pi.2)),
          pi_avg_median = rowMeans(cbind(.$median_pi.1, .$median_pi.2))) %>% 
-  mutate(watershed_diff= abs(watershed_2 - watershed_1)) %>% 
   select(-contains(".x"), -contains(".y"))
 
-#write_tsv(ani_sum, "/Users/kbg/Documents/UC_Berkeley/CyanoMeta_NSF/Metagenomics/Microcoleus_Analysis/Data/inStrain_data/ani_summary.tsv")
+write_tsv(ani_sum, "Data/inStrain_data/ani_summary_TEST.tsv")
 
 
 ## dRep results REMOVEE FROM ANALYSES
@@ -124,6 +119,9 @@ ani_sum <- comp_sp1.F %>%
 #   mutate(ani99_dRep= ifelse(ani_dRep > 0.995, ">0.995", 
 #                             ifelse(ani_dRep <= 0.995 & ani_dRep >=0.99, "0.99-0.995", "<0.99")))
 
+# DREP ANI JOIN
+#left_join(., sp1.ani) %>% 
+
 ## Haplotype frequencies REMOVE FROM ANALYSES
 # haplos <- read_tsv(file= "inStrain/output_tables/haplotype_freqs.tsv") %>% 
 #   filter(species == "species_1")
@@ -132,5 +130,11 @@ ani_sum <- comp_sp1.F %>%
 # snv_genomes <- read_tsv("Output_tables/snvs_genome_summary.tsv") %>% 
 #   filter(species == "species_1") %>% 
 #   select(ggkbase_id, SNV_mbp, pop_age)
+
+#rename(#pop_age.1= pop_age.x, pop_age.2= pop_age.y,
+#       SNV_mbp.1= SNV_mbp.x, SNV_mbp.2= SNV_mbp.y) %>% 
+#mutate(pop_age_pair= str_c(pop_age.1, pop_age.2, sep= "-")) %>%
+#mutate(pop_age_pair= ifelse(pop_age_pair == "Young-Old", "Old-Young", pop_age_pair)) %>% 
+
 
 
